07d983056e75f83c47db4eae3fddeed9795859af
CAFUCH <tjfdk6201@gmail.com>
2023-11-04 01:16:16 +0900

    Recipe SO
    Recipe SO 개발 중

---
+++ GGM_Graduation_Project/Assets/02. Scripts/SoalAh/RecipeSO.cs
@@ -0,0 +1,25 @@
+using BehaviorTree;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEditorInternal;
+using UnityEngine;
+
+//public struct OBJ
+//{
+//    List<ObjectType> processed;
+//    List<DvcType> device;
+//    List<GameObject> image;
+//}
+
+[System.Serializable]
+[CreateAssetMenu(menuName = "SO/Recipe", fileName = "Recipe_Data")]
+public class RecipeSO : ScriptableObject
+{
+    public GameObject obj;
+    public ObjectType objType;
+    public List<RecipeSO> source;
+    public List<DvcType> device;
+    // 이미지
+    // 오브젝트 (시작 재료, 중간,,... 에 맞는 조리 기구, 이미지)
+}
