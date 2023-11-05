55d0c96a15601a9ba28fdf26ba1a6536d5ba10c2
CAFUCH <tjfdk6201@gmail.com>
2023-11-06 00:50:39 +0900

    AI fix, throw out 상태 추가
    오브젝트 수리, 쓰레기통 사용 상태 추가

--- GGM_Graduation_Project/Assets/02. Scripts/SoalAh/FSM/Actions/FSM_ThrowOutAction.cs
+++ GGM_Graduation_Project/Assets/02. Scripts/SoalAh/FSM/Actions/FSM_ThrowOutAction.cs
@@ -1,12 +1,11 @@
-using BehaviorTree;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class FSM_MakeAction : FSM_Action
+public class FSM_ThrowOutAction : FSM_Action
 {
     public override void Action()
     {
-        Debug.Log("제작 액션 실행중");
+        Debug.Log("버리기 액션 실행중");
     }
 }
