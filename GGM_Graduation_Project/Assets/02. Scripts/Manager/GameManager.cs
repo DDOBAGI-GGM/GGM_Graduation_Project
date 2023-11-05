b227947c4464a0f31fb3405a7a4cf4f0a87fece5
Chae min seo <98958229+ms9876@users.noreply.github.com>
2023-11-06 00:27:53 +0900

    Gauge bar

--- GGM_Graduation_Project/Assets/02. Scripts/Manager/GameManager.cs
+++ GGM_Graduation_Project/Assets/02. Scripts/Manager/GameManager.cs
@@ -5,18 +5,5 @@ using UnityEngine.UI;
 
 public class GameManager : MonoBehaviour
 {
-    public float aiHp;
-    public float playerHp;
 
-    [SerializeField] private float maxHp;
-
-    private void Start()
-    {
-        aiHp = playerHp = maxHp;
-    }
-
-    private void SetHP()
-    {
-
-    }
 }
