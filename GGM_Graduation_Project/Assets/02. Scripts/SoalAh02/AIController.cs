6951e8d588d453771b22c0e609e466f371377cb9
CAFUCH <tjfdk6201@gmail.com>
2023-11-30 00:43:25 +0900

    AI 공부
    어렵습니다...
    default(T)는 T의 기본 값을 반환하는 것?

--- GGM_Graduation_Project/Assets/02. Scripts/SoalAh02/AIController.cs
+++ GGM_Graduation_Project/Assets/02. Scripts/SoalAh02/AIController.cs
@@ -2,11 +2,22 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.XR;
+using UnityEngine.AI;
 
+[RequireComponent(typeof(NavMeshAgent))]
 public class AIController : MonoBehaviour
 {
     private FSM<IState> stateManager;
     private BehaviourTreeManager<INode> btManager;
+
+    private NavMeshAgent agent;
+
+    private Transform destination;
+
+    private void Awake()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
 
     private void Start()
     {
@@ -36,3 +47,8 @@ public class AIController : MonoBehaviour
         Debug.Log("이동중");
     }
 }
+
+// 거리 체크 (nav, destination)
+// 손 체크 (hand, type?)
+// 고장 체크 (destination의 is머시기...)
+// 액션 (실행)
