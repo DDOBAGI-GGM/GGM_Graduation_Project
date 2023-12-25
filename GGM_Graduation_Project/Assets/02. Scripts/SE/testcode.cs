d05a58712d44295af1e22c502c7051f15ff1ca4c
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2023-12-25 23:59:49 +0900

    씬 변경 오류 해결
    - 돈 디스트로이 적용 완료

--- GGM_Graduation_Project/Assets/02. Scripts/SE/testcode.cs
+++ GGM_Graduation_Project/Assets/02. Scripts/SE/testcode.cs
@@ -1,60 +1,76 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
-
-/*public class Recipe : ScriptableObject
-{
-    public GameObject completion;           // 무기
-    public List<GameObject> items;          // 필요한 상태들?
-}*/
 
 public class testcode : MonoBehaviour
 {
-   // HashSet<Recipe> recipes = new HashSet<Recipe>();
+    float _time = 0;
+    public Slider _slider;
 
     private void Start()
     {
-      //  recipes.Add(new Recipe());
+      //  _slider = FindObjectOfType<Slider>();
+        //StartLoading("StageSelect_Scene");
     }
 
-
-    public static testcode instance;
-
-    public static testcode Instance { get { return instance; } }
-
-    public Button button;
-
-    private void Update()
+    public void StartLoading(string sceneName)
     {
-        if (Input.GetKeyDown(KeyCode.Escape))
-        {
-            dd();
-            Debug.Log("tlqk");
-        }
-
-        if (Input.GetKey(KeyCode.F))
-        {
-            button.interactable = false;
-        }
-        else
-        {
-            button.interactable = true;
-        }
+        StartCoroutine(LoadAsyncSceneCoroutine(sceneName));
     }
 
-    GameObject a;
-
-    private void dd()
+    public IEnumerator LoadAsyncSceneCoroutine(string sceneName)
     {
-     /*   var scripts = a.GetComponents<MonoBehaviour>();
-        foreach (var script in scripts)
+        Debug.Log("df");
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneName);
+        operation.allowSceneActivation = false;
+
+        while (!operation.isDone)
         {
-            if (script.GetType().IsSubclassOf(typeof(Ingredient)))
+            //_time += Time.deltaTime;
+            _time += 0.1f;
+
+            _slider.value = _time / 10f;
+
+            if (_time > 10)
             {
-                // 원하는 처리를 수행합니다.
+                operation.allowSceneActivation = true;
             }
-        }*/
 
+            yield return new WaitForSeconds(0.1f);
+        }
+
+        //while (!operation.isDone)
+        //{
+        //    yield return null;
+
+        //    _time += Time.deltaTime;
+
+        //    if (operation.progress < 0.9f)      // 로드되는 맵이 클 때
+        //    {
+        //        Debug.Log("로드되는 맵이 좀 커서");
+        //        _slider.value = Mathf.Lerp(_slider.value, operation.progress, _time);
+        //        if (_slider.value >= operation.progress)
+        //        {
+        //            _time = 0f;
+        //        }
+        //    }
+        //    else
+        //    {
+        //        _slider.value = Mathf.Lerp(_slider.value, 1f, _time);
+        //        Debug.Log("아 여기서 그래되니");
+        //        if (_slider.value == 1.0f)      // 1초는 이 로딩 씬에서 있어야 해.
+        //        {
+        //            operation.allowSceneActivation = true;
+        //            CloudManager.Instance?.gameObject.SetActive(true);
+        //            CloudManager.Instance?.Move(false);
+        //            Debug.Log("씬 이동되고 구름 보여지기!");
+        //            yield break;
+        //        }
+        //    }
+        //}
+
+        //yield return null;
     }
 }
