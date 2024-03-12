0b602427bff0ca9557f743c8c0946cb33f6de33d
lIo0O0oIl <tjddms0923tjddms@gmail.com>
2024-03-12 18:31:24 +0900

    스테이지 셀랙트 씬 UI 변경
    - 내부적으로도 리솔트씬, SO 등 변경함.

--- GGM_Graduation_Project/Assets/02. Scripts/SE/UI/TimeOutManager.cs
+++ GGM_Graduation_Project/Assets/02. Scripts/SE/UI/TimeOutManager.cs
@@ -6,23 +6,19 @@ using UnityEngine.SceneManagement;
 
 public class TimeOutManager : MonoBehaviour
 {
+    public static TimeOutManager instance;
+
     [SerializeField] GameObject timeOutPanel;
     [SerializeField] float animTime = 1f;
-    //public GameObject just;     // 지워도됨.시간 테스트용으로 해둔 것임.
 
-    private void Update()
+    private void Awake()
     {
-       // just.transform.Translate(new Vector3(-1 * Time.deltaTime, 0, 0));
-        if (Input.GetKeyDown(KeyCode.T))
-        {
-            timeOutShow();
-        }
+        instance = this;
     }
 
     Tween to;
-    public void timeOutShow()
+    public void TimeOutShow()
     {
-
         timeOutPanel.transform.DOScale(new Vector3(1, 1, 1), animTime / 2).SetEase(Ease.OutBack).OnComplete(() => StartCoroutine(LoadStageResultScene()));
         to = DOTween.To(() => Time.timeScale, scale => Time.timeScale = scale, 0, animTime);
         to.Play();
@@ -33,7 +29,9 @@ public class TimeOutManager : MonoBehaviour
 
         yield return new WaitForSeconds(animTime / 10f);     // 플레이어 멈춤 만들면 /2 로 바꾸어주기
         // 플레이어의 이동 멈춰주기
-        Debug.Log("플레이어 이동 멈춰주십쇼");
+        Debug.Log("플레이어 이동 멈춰주고 퍼센트 다시 셋팅");
+        HP.Instance.Gage.value = GameManager.Instance.nowStageData.myPersent;
+        GameManager.Instance.nowStageData.PersentSetting();
        to.Kill();
 
         Time.timeScale = 1f;
@@ -45,7 +43,6 @@ public class TimeOutManager : MonoBehaviour
 
         while (!operation.isDone)
         {
-            //Debug.Log(operation.progress);
             if (operation.progress >= 0.9f)      // 로드가 다 되었을 때
             {
                 SettingManager.Instance?.FadeAnim(false);
